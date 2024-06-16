a572a52b2b78ec7415531096b8c9e52b787b2ece
Volodya Bublyk <151150647+bublyk01@users.noreply.github.com>
2024-06-16 13:47:53 +0300

    Image was converted to a black and white version

--- Lab2/part2.py
+++ Lab2/part2.py
@@ -5,7 +5,22 @@ import matplotlib.pyplot as plt
 image_path = 'image.jpg'
 image_raw = imread(image_path)
 
-print(image_raw.shape)
+print(f"Original image: {image_raw.shape}")
 
+image_sum = image_raw.sum(axis=2)
+
+image_bw = image_sum / image_sum.max()
+
+print(f"B&W image: {image_bw.shape}")
+
+print(f"Maximum value in black and white image: {image_bw.max()}")
+
+plt.figure(figsize=(8, 4))
+plt.subplot(1, 2, 1)
 plt.imshow(image_raw)
+plt.title("Original image")
+
+plt.subplot(1, 2, 2)
+plt.imshow(image_bw, cmap='gray')
+plt.title("B&W image")
 plt.show()
